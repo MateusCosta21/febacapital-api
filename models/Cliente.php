161e7e80d5270a2e09593a4905f8f55adab1d7b4
mateuscosta <jomateuscs@gmail.com>
2024-09-13 19:36:20 -0300

    [feature] Authjwt
    Integração cliente feito

--- models/Cliente.php
+++ models/Cliente.php
@@ -4,7 +4,6 @@ namespace app\models;
 
 use Yii;
 use yii\db\ActiveRecord;
-use app\components\services\CepService;
 
 class Cliente extends ActiveRecord
 {
@@ -16,8 +15,9 @@ class Cliente extends ActiveRecord
     public function rules()
     {
         return [
-            [['nome', 'cpf', 'endereco', 'sexo'], 'required'],
+            [['nome', 'cpf', 'endereco', 'sexo'], 'required', 'message' => 'Este campo é obrigatório.'],
             ['cpf', 'match', 'pattern' => '/^\d{3}\.\d{3}\.\d{3}-\d{2}$/', 'message' => 'CPF inválido. O formato deve ser xxx.xxx.xxx-xx'],
+            ['cpf', 'validateUniqueCpf'], 
             [['endereco'], 'string'],
             [['cep'], 'string', 'max' => 10],
             [['logradouro'], 'string', 'max' => 255],
@@ -27,6 +27,20 @@ class Cliente extends ActiveRecord
             [['nome'], 'string', 'max' => 255],
         ];
     }
-    
 
+    public function validateUniqueCpf($attribute, $params)
+    {
+        $query = self::find()->where(['cpf' => $this->cpf]);
+    
+        if (!$this->isNewRecord) {
+            $query->andWhere(['!=', 'id', $this->id]);
+        }
+    
+        $existingCliente = $query->one();
+    
+        if ($existingCliente) {
+            $this->addError($attribute, 'Já existe um cliente com esse CPF.');
+        }
+    }
+    
 }
